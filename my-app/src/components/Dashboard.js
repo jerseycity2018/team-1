4803dc3e37c7f0917c7f9177b34c9dbdfe189c24
Christiantav <chrisadrianotav@gmail.com>
2018-10-27 06:56:53 -0400

    map n stuff

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button, FormGroup, FormControl, ControlLabel, Jumbotron, Navbar, Nav, NavItem } from "react-bootstrap";
+import SimpleMap from './Map';
 
 class Dashboard extends Component {
   render() {
@@ -10,7 +11,7 @@ class Dashboard extends Component {
             Central Park Statistics
         </header>
         </div>
-
+        <SimpleMap />
       </div>
     );
   }
