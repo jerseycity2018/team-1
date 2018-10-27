1d72b8bc3d0a040253d75acc2413a68eea34b504
Stella Ma <stellama98@gmail.com>
2018-10-27 01:31:24 -0400

    Fixed margins and header

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -7,23 +7,24 @@ class Dashboard extends Component {
     return (
       <div>
       <div>
-        <header>
+        <header className="headerstyle">
             Central Park Statistics
         </header>
         </div>
-        <Grid>
 
+        <Grid className="top-buffer">
           <Row className="show-grid" className="border">
             <Col md={12}>
               <code>{'<Col md={12} />'}</code>
             </Col>
           </Row>
-          <Row>
+          <Row className="top-buffer">
             <Col xs={12} md={3} className="border">
               <code>{'<Col xs={12} md={4} />'}</code>
             </Col>
-            <Col xs={12} md={4} xsOffset={1} className="border">
-              <code>{'<Col xs={12} md={4} />'}</code>
+            <Col xs={12} md={4} xsOffset={1} className="border subtitles">
+              <h4>Group Size</h4>
+
             </Col>
             <Col xs={12} md={3} mdOffset={1} className="border">
               <code>{'<Col xs={12} md={4} />'}</code>
