5b2312d4041b4397595cb76ccb93feb797af4f3e
Stella Ma <stellama98@gmail.com>
2018-10-26 23:19:51 -0400

    Added beginning of grid

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, FormControl, ControlLabel, Jumbotron, Navbar, Nav, NavItem } from "react-bootstrap";
+import { Button, FormGroup, Grid, Row, Col, FormControl, ControlLabel, Jumbotron, Navbar, Nav, NavItem } from "react-bootstrap";
+import './../styles/dashboard.css';
 
 class Dashboard extends Component {
   render() {
@@ -10,7 +11,24 @@ class Dashboard extends Component {
             Central Park Statistics
         </header>
         </div>
-
+        <Grid>
+          <Row className="show-grid">
+            <Col md={12}>
+              <code>{'<Col md={12} />'}</code>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={3}>
+              <code>{'<Col xs={12} md={4} />'}</code>
+            </Col>
+            <Col xs={12} md={3} xsOffset={1}>
+              <code>{'<Col xs={12} md={4} />'}</code>
+            </Col>
+            <Col xs={12} md={3} xsOffset={1}>
+              <code>{'<Col xs={12} md={4} />'}</code>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
