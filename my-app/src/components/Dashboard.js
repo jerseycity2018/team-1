8fea183693aa5f2937dbcfe5ca36b05fcf4b7ba2
Stella Ma <stellama98@gmail.com>
2018-10-27 01:58:47 -0400

    Added other sections and rounded borders

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -15,19 +15,29 @@ class Dashboard extends Component {
         <Grid className="top-buffer">
           <Row className="show-grid" className="border">
             <Col md={12}>
-              <code>{'<Col md={12} />'}</code>
+              <h4 className="subtitles">Demographics</h4>
             </Col>
           </Row>
           <Row className="top-buffer">
             <Col xs={12} md={3} className="border">
-              <code>{'<Col xs={12} md={4} />'}</code>
+              <h4 className="subtitles">Location</h4>
             </Col>
-            <Col xs={12} md={4} xsOffset={1} className="border subtitles">
-              <h4>Group Size</h4>
-
+            <Col xs={12} md={4} xsOffset={1} className="border">
+              <h4 className="subtitles">Group Size</h4>
             </Col>
             <Col xs={12} md={3} mdOffset={1} className="border">
-              <code>{'<Col xs={12} md={4} />'}</code>
+              <h4 className="subtitles">Time</h4>
+            </Col>
+          </Row>
+          <Row className="top-buffer">
+            <Col xs={12} md={3} className="border">
+              <h4 className="subtitles">Activities</h4>
+            </Col>
+            <Col xs={12} md={4} xsOffset={1} className="border">
+              <h4 className="subtitles">Places Visited</h4>
+            </Col>
+            <Col xs={12} md={3} mdOffset={1} className="border">
+              <h4 className="subtitles">Weather</h4>
             </Col>
           </Row>
         </Grid>
