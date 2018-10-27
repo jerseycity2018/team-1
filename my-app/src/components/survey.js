29d40b21e9c970b985bb751482e5d75e545a8913
Jasmine <jaznwong@gmail.com>
2018-10-27 01:18:58 -0400

    added details w/ Steven's changes

--- my-app/src/components/survey.js
+++ my-app/src/components/survey.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Button, ButtonToolbar, FormGroup, FormControl, Jumbotron, MenuItem, SplitButton
- } from "react-bootstrap";
+import { Button, ButtonToolbar, FormGroup, FormControl, ControlLabel, Jumbotron, Navbar, Nav, NavItem, MenuItem,
+  SplitButton, Row, Checkbox, Col} from "react-bootstrap";
 
 class Survey extends Component {
   constructor(props, context) {
@@ -47,13 +47,12 @@ handleKeyPress = (e) => {
       this.setState({zipcode: false});
       this.setState({group: true});
     }
-}	
+}
 
 getValidationState() {
   const length = this.state.value.length;
-  if (length > 10) return 'success';
-  else if (length > 5) return 'warning';
-  else if (length > 0) return 'error';
+  if (length == 10) return 'success';
+  else if (length != 5) return 'warning';
   return null;
 }
 
@@ -78,7 +77,7 @@ handleChange(e) {
             </Jumbotron>
             </div>
         ):<div></div>}
-        
+
         {this.state.renderZipcode ?(
         <div>
             <Jumbotron>
@@ -103,22 +102,38 @@ handleChange(e) {
         ):<div></div>}
 
         {this.state.renderCountry ?(
-        <div>        
+        <div>
         <Jumbotron>
           <h2> What country are you from? </h2>
-            <SplitButton title="Country" pullRight id="split-button-pull-right" bsSize="large">
-            <MenuItem eventKey="1">Action</MenuItem>
-            <MenuItem eventKey="2">Another action</MenuItem>
-            <MenuItem eventKey="3">Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey="4">Separated link</MenuItem>
-            </SplitButton>
+          <SplitButton title="Choose a Country" pullRight id="split-button-pull-right" bsSize="large">
+          <MenuItem eventKey="1">Afghanistan</MenuItem>
+          <MenuItem eventKey="2">Australia</MenuItem>
+          <MenuItem eventKey="3">Belgium</MenuItem>
+          <MenuItem eventKey="4">Brazil</MenuItem>
+          <MenuItem eventKey="5">Cambodia</MenuItem>
+          <MenuItem eventKey="6">Chile</MenuItem>
+          <MenuItem eventKey="7">Republic of China</MenuItem>
+          <MenuItem eventKey="8">Denmark</MenuItem>
+          <MenuItem eventKey="9">Dominican Republic</MenuItem>
+          <MenuItem eventKey="10">Egypt</MenuItem>
+          <MenuItem eventKey="11">Ethiopia</MenuItem>
+          <MenuItem eventKey="12">Finland</MenuItem>
+          <MenuItem eventKey="13">France</MenuItem>
+          <MenuItem eventKey="14">Germany</MenuItem>
+          <MenuItem eventKey="15">Guatemala</MenuItem>
+          <MenuItem eventKey="16">Haiti</MenuItem>
+          <MenuItem eventKey="17">Hungary</MenuItem>
+          <MenuItem eventKey="18">India</MenuItem>
+          <MenuItem eventKey="19">Iran</MenuItem>
+          <MenuItem eventKey="20">Japan</MenuItem>
+
+          </SplitButton>
         </Jumbotron>
         </div>
         ):<div></div>}
-        
+
         {this.state.renderGroup ?(
-        <div>             
+        <div>
         <Jumbotron>
           <h2> Are you alone or with a group? </h2>
             <ButtonToolbar>
@@ -131,89 +146,120 @@ handleChange(e) {
 
         <Jumbotron>
           <h2> How many people? (Including you) </h2>
-          <SplitButton title="Dropdown right" pullRight id="split-button-pull-right" bsSize="large">
-          <MenuItem eventKey="1">Action</MenuItem>
-          <MenuItem eventKey="2">Another action</MenuItem>
-          <MenuItem eventKey="3">Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey="4">Separated link</MenuItem>
-          </SplitButton>
+          <form>
+            <FormControl
+              type="text"
+              value={this.state.value}
+              placeholder="Enter number of people"
+              onChange={this.handleChange}
+            />
+            <FormControl.Feedback />
+        </form>
         </Jumbotron>
 
         <Jumbotron>
           <h2> Where did you go? </h2>
-          <SplitButton title="Dropdown right" pullRight id="split-button-pull-right" bsSize="large">
-          <MenuItem eventKey="1">Action</MenuItem>
-          <MenuItem eventKey="2">Another action</MenuItem>
-          <MenuItem eventKey="3">Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey="4">Separated link</MenuItem>
-          </SplitButton>
+          <FormGroup style={{marginLeft:"300px", marginRight:"300px"}}>
+          <Row className="show-grid">
+              <Col md={6} mdPush={6}>
+              <Checkbox>Cherry Hill</Checkbox>
+              <Checkbox>Strawberry Fields (Imagine Mosaic)</Checkbox>
+              <Checkbox>Bow Bridge</Checkbox>
+              <Checkbox>Shakespeare Garden</Checkbox>
+              <Checkbox>The Pool</Checkbox>
+              <Checkbox>The Loch</Checkbox>
+              <Checkbox>The Ravine</Checkbox>
+              <Checkbox>The Mall (Literary Walk)</Checkbox>
+              </Col>
+              <Col md={6} mdPull={6}>
+              <Checkbox>Balto</Checkbox>
+              <Checkbox>Bethesda Terrace (Bethesda Fountain)</Checkbox>
+              <Checkbox>Alice in Wonderland</Checkbox>
+              <Checkbox>Conservatory Water</Checkbox>
+              <Checkbox>Obelisk (Cleopatra's Needle)</Checkbox>
+              <Checkbox>Reservoir</Checkbox>
+              <Checkbox>Conservatory Garden</Checkbox>
+              <Checkbox>The Harlem Meer</Checkbox>
+              </Col>
+            </Row>
+            </FormGroup>
         </Jumbotron>
 
         <Jumbotron>
           <h2> What activities did you do? </h2>
-          <SplitButton title="Dropdown right" pullRight id="split-button-pull-right" bsSize="large">
-          <MenuItem eventKey="1">Action</MenuItem>
-          <MenuItem eventKey="2">Another action</MenuItem>
-          <MenuItem eventKey="3">Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey="4">Separated link</MenuItem>
-          </SplitButton>
+          <FormGroup style={{marginLeft:"500px", marginRight:"500px"}}>
+          <Row className="show-grid">
+              <Col md={6} mdPush={6}>
+              <Checkbox>Bicycle Rentals</Checkbox>
+              <Checkbox>Boating</Checkbox>
+              <Checkbox>Ice Skating</Checkbox>
+              <Checkbox>Swimming</Checkbox>
+              <Checkbox>Zoo</Checkbox>
+              </Col>
+              <Col md={6} mdPull={6}>
+              <Checkbox>Walk dog</Checkbox>
+              <Checkbox>Picnic</Checkbox>
+              <Checkbox>Play sports</Checkbox>
+              <Checkbox>Walk</Checkbox>
+              <Checkbox>Bike</Checkbox>
+              </Col>
+            </Row>
+
+            </FormGroup>
         </Jumbotron>
 
         <Jumbotron>
           <h2> How long did you stay for/plan to stay for? </h2>
-          <SplitButton title="Dropdown right" pullRight id="split-button-pull-right" bsSize="large">
-          <MenuItem eventKey="1">Action</MenuItem>
-          <MenuItem eventKey="2">Another action</MenuItem>
-          <MenuItem eventKey="3">Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey="4">Separated link</MenuItem>
-          </SplitButton>
-        </Jumbotron>
-
-        <Jumbotron>
-          <h2> How did you get to Central Park? </h2>
           <ButtonToolbar>
-            <Button bsSize="large" block>
-              Yes
-            </Button>
-            <Button bsSize="large" block>No</Button>
+            <Button bsSize="large" block>1 hour</Button>
+            <Button bsSize="large" block>2 hours</Button>
+            <Button bsSize="large" block>3 hours</Button>
+            <Button bsSize="large" block>4 hours</Button>
+            <Button bsSize="large" block>5+ hours</Button>
           </ButtonToolbar>
         </Jumbotron>
 
         <Jumbotron>
-          <h2> Age? </h2>
-          <SplitButton title="Dropdown right" pullRight id="split-button-pull-right" bsSize="large">
-          <MenuItem eventKey="1">Action</MenuItem>
-          <MenuItem eventKey="2">Another action</MenuItem>
-          <MenuItem eventKey="3">Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey="4">Separated link</MenuItem>
-          </SplitButton>
+          <h2> How did you get to Central Park? </h2>
+          <FormGroup style={{marginLeft:"300px", marginRight:"300px"}}>
+              <Checkbox>Subway</Checkbox>
+              <Checkbox>Long Island Railroad (LIRR)</Checkbox>
+              <Checkbox>Walking</Checkbox>
+              <Checkbox>Car</Checkbox>
+              <Checkbox>Biking</Checkbox>
+              <Checkbox>Other</Checkbox>
+            </FormGroup>
         </Jumbotron>
 
         <Jumbotron>
-          <h2> Gender? </h2>
-          <SplitButton title="Dropdown right" pullRight id="split-button-pull-right" bsSize="large">
-          <MenuItem eventKey="1">Action</MenuItem>
-          <MenuItem eventKey="2">Another action</MenuItem>
-          <MenuItem eventKey="3">Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey="4">Separated link</MenuItem>
-          </SplitButton>
+          <h2> Age </h2>
+          <ButtonToolbar>
+            <Button bsSize="large" block>0-15</Button>
+            <Button bsSize="large" block>15-24</Button>
+            <Button bsSize="large" block>25-34</Button>
+            <Button bsSize="large" block>35-44</Button>
+            <Button bsSize="large" block>45-55</Button>
+            <Button bsSize="large" block>55+</Button>
+          </ButtonToolbar>
         </Jumbotron>
 
         <Jumbotron>
-          <h2> Ethnicity? </h2>
-          <SplitButton title="Dropdown right" pullRight id="split-button-pull-right" bsSize="large">
-          <MenuItem eventKey="1">Action</MenuItem>
-          <MenuItem eventKey="2">Another action</MenuItem>
-          <MenuItem eventKey="3">Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey="4">Separated link</MenuItem>
-          </SplitButton>
+          <h2> Gender </h2>
+          <ButtonToolbar>
+            <Button bsSize="large" block>Male</Button>
+            <Button bsSize="large" block>Female</Button>
+            <Button bsSize="large" block>Other</Button>
+          </ButtonToolbar>
+        </Jumbotron>
+
+        <Jumbotron>
+          <h2> Ethnicity </h2>
+          <Button bsSize="large" block>White</Button>
+          <Button bsSize="large" block>Hispanic or Latino</Button>
+          <Button bsSize="large" block>Black or African American</Button>
+          <Button bsSize="large" block>Native American or American Indian</Button>
+          <Button bsSize="large" block>Asian/Pacific Islander</Button>
+          <Button bsSize="large" block>Other</Button>
         </Jumbotron>
       </div>
     )
