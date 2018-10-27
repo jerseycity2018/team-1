8660bd6e9624d369497e1439aabbd729f8c4acbb
Christiantav <chrisadrianotav@gmail.com>
2018-10-27 01:56:01 -0400

    routing and open weather map api updates

--- my-app/src/components/HomePage.js
+++ my-app/src/components/HomePage.js
@@ -1,19 +1,68 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, FormGroup, FormControl, ControlLabel, Jumbotron, Navbar, Nav, NavItem } from "react-bootstrap";
 
+
 class HomePage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            temperature: undefined,
+            icon: undefined
+        }
+    }
+
+        toFarenheit(kelvin) {
+            console.log(kelvin);
+            let temp = (kelvin - 273.15) * 9/5 + 32;
+            var result = parseInt(temp);
+            return result;
+        }
+        
+        getWeather = async () =>  {
+        
+            const weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=40.7829&lon=-73.9654&APPID=296bae7bdc80a2170e070a88bd45571d";
+        
+            const api_call = fetch(weatherURL).then(
+                response => response.json()).then(
+                    data => {
+                    
+                    console.log(data.weather.icon)
+                    
+                    this.setState({
+                    temperature: data.main.temp,
+                    icon: data.weather[0].icon,
+                    error: ""
+                })
+            }
+        );
+
+
+
+            //console.log(api_call);
+                      
+        }
+
+        componentDidMount () {
+            this.getWeather();
+        }
+    
   render() {
     return (
         <div>
-            <Jumbotron>    
-                <div class="intro-heading"><h1>Welcome to the Team!</h1></div>
-                <div class="intro-lead-in"><h3>Thank you for joining</h3></div> 
+            <Jumbotron>
+            <div loadWeather={this.getWeather}>
+    {this.state.temperature && <p>Temperature: {this.toFarenheit(this.state.temperature)} degrees</p>}
+            {this.state.icon && <img src={`http://openweathermap.org/img/w/${this.state.icon}.png`}/>}
+            </div>    
+                <div><h1>Welcome to Central Park!</h1></div>
+                <div><h3>Thank you for visiting</h3></div> 
             </Jumbotron>
             <ul>
-                <li><h4>This site is meant to help you manage your PIN Project. If you have formed a team and been approved, register your team and get cracking!</h4></li>
-                <li><h4>Want to know more? Check out our program packet <a href='https://docs.google.com/document/d/1tJVbdjhijTm4VF9-ub7bVDBm8r84s-AGqw4Py76LUms/edit?usp=sharing'>HERE</a></h4></li>
-                <li><h4>As a nonprofit, we are funded by the support of our stakeholders. Feel free to <a href='https://www.gofundme.com/project-innovate-newark'>DONATE</a></h4></li>
-                <li><h4>Check out our website and get all of your PINformation by clicking the button below!</h4></li>
+            <Button bsStyle="primary">Our Survey
+            <Link to="/survey"></Link>
+            </Button>
             </ul>
         </div>
     )
