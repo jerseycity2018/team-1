5c15e4de89dfeb114163103e9c1ca9e8acecb06a
Jasmine <jaznwong@gmail.com>
2018-10-27 02:30:33 -0400

    pretty styles

--- my-app/src/components/survey.js
+++ my-app/src/components/survey.js
@@ -63,25 +63,24 @@ handleChange(e) {
   render() {
     return (
       <div>
-        <Jumbotron>
             <h1> Survey </h1>
-        </Jumbotron>
         {this.state.renderStates ?(
             <div>
-            <Jumbotron>
-                <h2> Are you from the United States? </h2>
-                <ButtonToolbar>
+            <Jumbotron style={{backgroundColor:"#69ca6b"}}>
+                <h2 style={{color:"white"}}> Are you from the United States? </h2>
+                <div style={{width:"50%", marginLeft:"300px"}}>
                     <Button onClick={()=>this.handleClick(1)} value="yes" bsSize="large" block>Yes</Button>
                     <Button onClick={()=>this.handleClick(2)} value="no" bsSize="large" block>No</Button>
-                </ButtonToolbar>
+                </div>
             </Jumbotron>
             </div>
         ):<div></div>}
 
         {this.state.renderZipcode ?(
         <div>
-            <Jumbotron>
-                <h2> What is your Zip Code? </h2>
+            <Jumbotron style={{backgroundColor:"#69ca6b"}}>
+                <h2 style={{color:"white"}}> What is your Zip Code? </h2>
+                <div style={{width:"50%", marginLeft:"300px"}}>
                 <form>
                     <FormGroup
                     controlId="formBasicText"
@@ -97,35 +96,36 @@ handleChange(e) {
                     </FormGroup>
                 </form>
                 <button onClick={()=>this.handleClick(3)}>Enter</button>
+                </div>
             </Jumbotron>
         </div>
         ):<div></div>}
 
         {this.state.renderCountry ?(
         <div>
-        <Jumbotron>
-          <h2> What country are you from? </h2>
+        <Jumbotron style={{backgroundColor:"#69ca6b"}}>
+          <h2 style={{color:"white"}}> What country are you from? </h2>
           <SplitButton title="Choose a Country" pullRight id="split-button-pull-right" bsSize="large">
-          <MenuItem eventKey="1">Afghanistan</MenuItem>
-          <MenuItem eventKey="2">Australia</MenuItem>
-          <MenuItem eventKey="3">Belgium</MenuItem>
-          <MenuItem eventKey="4">Brazil</MenuItem>
-          <MenuItem eventKey="5">Cambodia</MenuItem>
-          <MenuItem eventKey="6">Chile</MenuItem>
-          <MenuItem eventKey="7">Republic of China</MenuItem>
-          <MenuItem eventKey="8">Denmark</MenuItem>
-          <MenuItem eventKey="9">Dominican Republic</MenuItem>
-          <MenuItem eventKey="10">Egypt</MenuItem>
-          <MenuItem eventKey="11">Ethiopia</MenuItem>
-          <MenuItem eventKey="12">Finland</MenuItem>
-          <MenuItem eventKey="13">France</MenuItem>
-          <MenuItem eventKey="14">Germany</MenuItem>
-          <MenuItem eventKey="15">Guatemala</MenuItem>
-          <MenuItem eventKey="16">Haiti</MenuItem>
-          <MenuItem eventKey="17">Hungary</MenuItem>
-          <MenuItem eventKey="18">India</MenuItem>
-          <MenuItem eventKey="19">Iran</MenuItem>
-          <MenuItem eventKey="20">Japan</MenuItem>
+          <MenuItem eventKey="Afghanistan">Afghanistan</MenuItem>
+          <MenuItem eventKey="Australia">Australia</MenuItem>
+          <MenuItem eventKey="Belgium">Belgium</MenuItem>
+          <MenuItem eventKey="Brazil">Brazil</MenuItem>
+          <MenuItem eventKey="Cambodia">Cambodia</MenuItem>
+          <MenuItem eventKey="Chile">Chile</MenuItem>
+          <MenuItem eventKey="Republic of China">Republic of China</MenuItem>
+          <MenuItem eventKey="Denmark">Denmark</MenuItem>
+          <MenuItem eventKey="Dominican Republic">Dominican Republic</MenuItem>
+          <MenuItem eventKey="Egypt">Egypt</MenuItem>
+          <MenuItem eventKey="Ethiopia">Ethiopia</MenuItem>
+          <MenuItem eventKey="Finland<">Finland</MenuItem>
+          <MenuItem eventKey="France">France</MenuItem>
+          <MenuItem eventKey="Germany">Germany</MenuItem>
+          <MenuItem eventKey="Guatemala">Guatemala</MenuItem>
+          <MenuItem eventKey="Haiti">Haiti</MenuItem>
+          <MenuItem eventKey="Hungary">Hungary</MenuItem>
+          <MenuItem eventKey="India">India</MenuItem>
+          <MenuItem eventKey="Iran">Iran</MenuItem>
+          <MenuItem eventKey="Japan">Japan</MenuItem>
 
           </SplitButton>
         </Jumbotron>
@@ -134,18 +134,19 @@ handleChange(e) {
 
         {this.state.renderGroup ?(
         <div>
-        <Jumbotron>
-          <h2> Are you alone or with a group? </h2>
-            <ButtonToolbar>
+        <Jumbotron style={{backgroundColor:"#69ca6b"}}>
+          <h2 style={{color:"white"}}> Are you alone or with a group? </h2>
+            <div style={{width:"50%", marginLeft:"300px"}}>
               <Button bsSize="large" block>Alone</Button>
               <Button bsSize="large" block>Group</Button>
-            </ButtonToolbar>
+            </div>
         </Jumbotron>
         </div>
         ):<div></div>}
 
-        <Jumbotron>
-          <h2> How many people? (Including you) </h2>
+        <Jumbotron style={{backgroundColor:"#69ca6b"}}>
+          <h2 style={{color:"white"}}> How many people? (Including you) </h2>
+          <div style={{width:"50%", marginLeft:"300px"}}>
           <form>
             <FormControl
               type="text"
@@ -155,10 +156,11 @@ handleChange(e) {
             />
             <FormControl.Feedback />
         </form>
+        </div>
         </Jumbotron>
 
-        <Jumbotron>
-          <h2> Where did you go? </h2>
+        <Jumbotron style={{backgroundColor:"#69ca6b"}}>
+          <h2 style={{color:"white"}}> What places did you visit? </h2>
           <FormGroup style={{marginLeft:"300px", marginRight:"300px"}}>
           <Row className="show-grid">
               <Col md={6} mdPush={6}>
@@ -185,81 +187,85 @@ handleChange(e) {
             </FormGroup>
         </Jumbotron>
 
-        <Jumbotron>
-          <h2> What activities did you do? </h2>
-          <FormGroup style={{marginLeft:"500px", marginRight:"500px"}}>
+        <Jumbotron style={{backgroundColor:"#69ca6b"}}>
+          <h2 style={{color:"white"}}> What activities did you do? </h2>
+          <FormGroup style={{marginLeft:"400px", marginRight:"400px"}}>
           <Row className="show-grid">
               <Col md={6} mdPush={6}>
-              <Checkbox>Bicycle Rentals</Checkbox>
-              <Checkbox>Boating</Checkbox>
-              <Checkbox>Ice Skating</Checkbox>
-              <Checkbox>Swimming</Checkbox>
-              <Checkbox>Zoo</Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Bicycle Rentals</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Boating</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Ice Skating</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Swimming</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Zoo</h4></Checkbox>
               </Col>
               <Col md={6} mdPull={6}>
-              <Checkbox>Walk dog</Checkbox>
-              <Checkbox>Picnic</Checkbox>
-              <Checkbox>Play sports</Checkbox>
-              <Checkbox>Walk</Checkbox>
-              <Checkbox>Bike</Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Walk dog(s)</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Picnic</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Play sports</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Walk</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Bike</h4></Checkbox>
               </Col>
             </Row>
 
             </FormGroup>
         </Jumbotron>
 
-        <Jumbotron>
-          <h2> How long did you stay for/plan to stay for? </h2>
-          <ButtonToolbar>
+        <Jumbotron style={{backgroundColor:"#69ca6b"}}>
+          <h2 style={{color:"white"}}> How long did you stay for/plan to stay for? </h2>
+          <div style={{width:"50%", marginLeft:"300px"}}>
             <Button bsSize="large" block>1 hour</Button>
             <Button bsSize="large" block>2 hours</Button>
             <Button bsSize="large" block>3 hours</Button>
             <Button bsSize="large" block>4 hours</Button>
             <Button bsSize="large" block>5+ hours</Button>
-          </ButtonToolbar>
+          </div>
         </Jumbotron>
 
-        <Jumbotron>
-          <h2> How did you get to Central Park? </h2>
+        <Jumbotron style={{backgroundColor:"#69ca6b"}}>
+          <h2 style={{color:"white"}}> How did you get to Central Park? </h2>
           <FormGroup style={{marginLeft:"300px", marginRight:"300px"}}>
-              <Checkbox>Subway</Checkbox>
-              <Checkbox>Long Island Railroad (LIRR)</Checkbox>
-              <Checkbox>Walking</Checkbox>
-              <Checkbox>Car</Checkbox>
-              <Checkbox>Biking</Checkbox>
-              <Checkbox>Other</Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Subway</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Long Island Railroad (LIRR)</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Walking</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Car</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Biking</h4></Checkbox>
+              <Checkbox> <h4 style={{display:"inline-block"}}>Other</h4></Checkbox>
             </FormGroup>
         </Jumbotron>
 
-        <Jumbotron>
-          <h2> Age </h2>
-          <ButtonToolbar>
+        <Jumbotron style={{backgroundColor:"#69ca6b"}}>
+          <h2 style={{color:"white"}}> Age </h2>
+          <div style={{width:"50%", marginLeft:"300px"}}>
             <Button bsSize="large" block>0-15</Button>
             <Button bsSize="large" block>15-24</Button>
             <Button bsSize="large" block>25-34</Button>
             <Button bsSize="large" block>35-44</Button>
             <Button bsSize="large" block>45-55</Button>
             <Button bsSize="large" block>55+</Button>
-          </ButtonToolbar>
+          </div>
         </Jumbotron>
 
-        <Jumbotron>
-          <h2> Gender </h2>
-          <ButtonToolbar>
+        <Jumbotron style={{backgroundColor:"#69ca6b"}}>
+          <h2 style={{color:"white"}}> Gender </h2>
+          <div style={{width:"50%", marginLeft:"300px"}}>
             <Button bsSize="large" block>Male</Button>
             <Button bsSize="large" block>Female</Button>
             <Button bsSize="large" block>Other</Button>
-          </ButtonToolbar>
+            <Button bsSize="large" block>Prefer to not disclose</Button>
+          </div>
         </Jumbotron>
 
-        <Jumbotron>
-          <h2> Ethnicity </h2>
+        <Jumbotron style={{backgroundColor:"#69ca6b"}}>
+          <h2 style={{color:"white"}}> Ethnicity </h2>
+          <div style={{width:"50%", marginLeft:"300px"}}>
           <Button bsSize="large" block>White</Button>
           <Button bsSize="large" block>Hispanic or Latino</Button>
           <Button bsSize="large" block>Black or African American</Button>
           <Button bsSize="large" block>Native American or American Indian</Button>
           <Button bsSize="large" block>Asian/Pacific Islander</Button>
           <Button bsSize="large" block>Other</Button>
+          <Button bsSize="large" block>Prefer to not disclose</Button>
+          </div>
         </Jumbotron>
       </div>
     )
