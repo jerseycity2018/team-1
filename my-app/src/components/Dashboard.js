e8cb0b0a503992532640f021b91d094ca88a85f7
Stella Ma <stellama98@gmail.com>
2018-10-26 21:11:15 -0400

    Added Dashboard files

---
+++ my-app/src/components/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <p>
+            Edit
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
